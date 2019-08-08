226c337d1c6e6c077b04c99080dba94a3055e64e
Philip Chwistek <mrsportsnerd@gmail.com>
2019-08-07 22:17:38 -0400

    added my content, just need to update constraints

--- eureka/README.md
+++ eureka/README.md
@@ -1,2 +1,5 @@
 # Eurkea Records  <#Title#>
 
+## Credits
+Record player icon by Freepik (https://www.flaticon.com/authors/freepik)
+Fonts: Lobster and Oswald 
--- eureka/ViewController.swift
+++ eureka/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import MapKit
+/* ref:
+ https://developer.apple.com/documentation/mapkit
+ https://www.ioscreator.com/tutorials/mapkit-ios-tutorial
+*/
+
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    let initialLocation = CLLocation(latitude: 40.310058, longitude: -75.129884)
+    
+    let regionRadius: CLLocationDistance = 750
+    
+    func prepareMap(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
+                                                  latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        let annotation = MKPointAnnotation()
 
+        mapView.setRegion(coordinateRegion, animated: true)
+        
+        annotation.coordinate = location.coordinate
+        annotation.title = "Eureka Records"
+        annotation.subtitle = "Our place!"
+        mapView.addAnnotation(annotation)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        prepareMap(location: initialLocation)
+        
+        
+      
+
     }
 
 
